b4af8da4ddc693980eb0e6329fe452516274e9f6
Gabriel <shalitmontagne.g@gmail.com>
2021-12-08 16:26:03 -0800

    commit tree 49069b952b8711303d9b977677fd55b52d00fbca

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [toggleCard, setToggleCart] = useState(false);
+  const buttonText = toggleCard ? "Remove From Cart" : "Add to Cart"
+  const liClass = toggleCard ? "in-cart" : ""
+  const buttonClass = toggleCard ? "remove" : "add"
+
+  function handleAddCart() {
+    setToggleCart(!toggleCard)
+  }
+
   return (
-    <li className="">
+    <li className={liClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleAddCart} className={buttonClass}>{buttonText}</button>
     </li>
   );
 }
